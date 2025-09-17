e252359b15126bfb53634260bd97a8cebd9d4dc4
Daniel Gomes <gomesdaniel@outlook.com>
2025-09-17 13:28:02 -0300

    commit

---
+++ ex7-ifelse.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("How old are you? ");
+    scanf("%*d", &age);
+
+    if (age >= 18) {
+        printf("You are an adult.\n");
+    } else {
+        printf("You are a minor.\n");
+    }
+    return 0;
+}
