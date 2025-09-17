097a90ef90dc8b16752dfce58df175fd3751c094
mcloir <gomesdaniel@outlook.com>
2025-09-17 01:33:59 -0300

    changes.

--- ex6 tempconvert.cpp
+++ ex6 tempconvert.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main () {
+    setlocale(LC_ALL, "en_US.UTF-8");
+
     float c, k, f;
     int choice;
 
@@ -14,13 +17,52 @@ int main () {
 
     scanf("%d", &choice);
 
-    switch (expression)
+    switch (choice)
     {
-    case constant expression:
-        /* code */
+    case 1:
+        printf("Please, enter the temperature in Celsius: ");
+        scanf("%f", &c);
+         k = c + 273.15;
+         printf("The temperature in Kelvin is: %.2f K\n", k);
         break;
     
+    case 2:
+        printf("Please, enter the temperature in Kelvin: ");
+        scanf("%f", &k);
+        c = k - 273.15;
+        printf("The temperature in Celsius is: %.2f °C\n", c);
+        break;
+    
+    case 3:
+        printf("Please, enter the temperature in Celsius: ");
+        scanf("%f", &c);
+        f = 9*c/5 + 32;
+        printf("The temperature in Fahrenheit is: %.2f °F\n", f);
+        break;
+    
+    case 4:
+        printf("Please, enter the temperature in Fahrenheit: ");
+        scanf("%f", &f);
+        c = 5 * (f - 32) / 9;
+        printf("The temperature in Celsius is: %.2f °C\n", c);
+        break;
+    
+    case 5:
+        printf("Please, enter the temperature in Kelvin: ");
+        scanf("%f", &k);
+        f = 9 * (k - 273.15) / 5 + 32;
+        printf("The temperature in Fahrenheit is: %.2f °F\n", f);
+        break;
+    
+    case 6:
+        printf("Please, enter the temperature in Fahrenheit: ");
+        scanf("%f", &f);
+        k = 5 * (f - 32) / 9 + 273.15;
+        printf("The temperature in Kelvin is: %.2f K\n", k);
+        break;     
+    
     default:
+        printf("Invalid choice.\n");
         break;
     }
     return 0;
