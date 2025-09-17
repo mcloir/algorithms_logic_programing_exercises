4cbb2195ac6335ea68e2557bc387fc66e72446e8
mcloir <gomesdaniel@outlook.com>
2025-09-16 23:39:43 -0300

    temp converter

---
+++ ex6 tempconvert.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main () {
+    float c, k, f;
+    int choice;
+
+    printf("Hello, my frind! Please, pick a number concerning the temperature conversion that you want to do:\n");
+    printf("1 - Celsius to Kelvin\n");
+    printf("2 - Kelvin to Celsius\n");
+    printf("3 - Celsius to Fahrenheit\n");
+    printf("4 - Fahrenheit to Celsius\n");
+    printf("5 - Kelvin to Fahrenheit\n");
+    printf("6 - Fahrenheit to Kelvin\n");
+
+    scanf("%d", &choice);
+
+    switch (expression)
+    {
+    case constant expression:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+    return 0;
+}
