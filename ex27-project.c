f87fd8927084804ad03a6a1a53de89465ee2183b
mcloir <gomesdaniel@outlook.com>
2025-09-25 22:00:37 -0300

    update

--- ex27-project.c
+++ ex27-project.c
@@ -10,45 +10,50 @@ struct book {
     char title[max_name];
     char author[max_name];
     int year;
-    int availability;
+    int availability; // 1 = disponível, 0 = emprestado
 };
 
 struct book library [max_books];
 int total_books = 0;
 
-void load_biblio(void);
-void save_biblio(void);
-void add_books(void);
-int search_book(int id);
-void lend_book(void);
-void return_book(void);
-void main_menu(void);
+void load(void);
+void save(void) {
+    printf("Saving\n");
+}
+void add(void);
+void list(void);
+int search(int id);
+void lend(void);
+void returning(void);
+void mainmenu(void) {
+    printf("Main Menu\n");
+}
 
 int main () {
-    load_biblio();
-    main_menu();
-    save_biblio();
+    load();
+    mainmenu();
+    save();
 
     return 0;
 
 }
 
-void load_biblio(void) {
-    FILE *file = fopen("biblio.txt", "r");
+void load(void) {
+    FILE *file = fopen("library.txt", "r");
 
     if (file == NULL) {
-        printf("File not found, initializing empty library");
+        printf("File not found, initializing an empty library.\n");
         return;
     }
 
-        while (fscanf(file, "%d %s %s %d %d",
-            &library[total_books].id,
-            library[total_books].title,
-            library[total_books].author,
-            &library[total_books].year,
-            &library[total_books].availability) == 5 && total_books < max_books) {
-                total_books++;
-            }
-        fclose(file);
-        printf("Library loaded with %d book(s).\n", total_books);
+    while (fscanf(file, "%d %s %s %d %d",
+        &library[total_books].id,
+        library[total_books].title,
+        library[total_books].author,
+        &library[total_books].year,
+        &library[total_books].availability) == 5 && total_books < max_books) {
+        total_books++;
+        }
+    fclose(file);
+    printf("Library loaded with %d book(s).\n", total_books);
 }
